5b71c6ee4a890145e9990ef1002aec4183bcc7b2
VinicMello <mellopedroso10@gmail.com>
2025-04-09 23:01:00 -0300

    [feature] - Novos atributos e repositórios

--- etl/extract.py
+++ etl/extract.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 TOKEN = os.getenv("GITHUB_TOKEN")
+URL = 'https://api.github.com'
 
 class Dados:
 
@@ -10,34 +11,63 @@ class Dados:
         """
         """
         self.owner = owner
-        self.url = 'https://api.github.com'
         self.token = TOKEN
         self.headers = {'Authorization': 'Bearer ' + self.token,
                         'X-GitHub-Api-Version': '2022-11-28'}
+        self._overview_data = self._get_overview_data() 
+        self.url_profile = self.profile_url
+        self.overview = self.format_overview_profile
     
     def check_api_status(self):
         """
         """
         try:
-            return requests.get(url=self.url, headers=self.headers).status_code == 200
+            return requests.get(url=URL, headers=self.headers).status_code == 200
         except requests.exceptions.RequestException as error:
             raise error
     
-    def overview_rep(self):
+    def _get_overview_data(self):
         """
         """
         try:
-            url = f'{self.url}/users/{self.owner}'
+            url = f'{URL}/users/{self.owner}'
             response = requests.get(url=url, headers=self.headers)
-            return json.dumps(response.json(), indent=4)
+            return response.json()
         except requests.exceptions.RequestException as error:
             raise error
+    
+    @property
+    def format_overview_profile(self):
+        """
+        """
+        return json.dumps(self._get_overview_data(), indent=4)
+    
+    @property
+    def profile_url(self):
+        """
+        """
+        return self._overview_data.get("html_url")
+
+    def get_repos(self):
+        """
+        """
+        url_rep = self._overview_data.get("repos_url")
+        print()
 
     
-my_rep = Dados("VinicMello")
-print(my_rep.token)
-print(my_rep.check_api_status())
-print(my_rep.overview_rep())
+my_rep = Dados("Netflix")
+print(my_rep.overview)
+# print(my_rep.token)
+# print(my_rep.check_api_status())
+# rep = my_rep.overview_rep()
+
+# url_rep = rep['repos_url']
+# response = requests.get(url=url_rep, headers=my_rep.headers)
+
+# teste = response.json()
+# for rep in teste:
+#     print(rep['name'])
+print()
 
 
 
